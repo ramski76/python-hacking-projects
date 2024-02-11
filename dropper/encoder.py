f0747b21d80a814186e8e7ced119d365633a4c1a
ramski76 <146506235+ramski76@users.noreply.github.com>
2024-02-11 15:55:52 +0530

    Update encoder.py

--- dropper/encoder.py
+++ dropper/encoder.py
@@ -2,6 +2,6 @@ from stegano import lsb
 
 message = 'print("Hello World")'
 
-image = lsb.hide(image=r'image.png', message=message)
+image = lsb.hide(image=r'/path/to/normal/image', message=message)
 
-image.save(r"E:\python-programs\dropper\workspace\eimage.png")
+image.save(r"/path/to/save/encoded/image")
