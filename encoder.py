6aafddeb6cadaafdea2f5351470e2c17403c317a
ramski76 <146506235+ramski76@users.noreply.github.com>
2024-02-11 15:50:26 +0530

    Delete encoder.py

--- encoder.py
+++
@@ -1,7 +0,0 @@
-from stegano import lsb
-
-message = 'print("Hello World")'
-
-image = lsb.hide(image=r'image.png', message=message)
-
-image.save(r"E:\python-programs\dropper\workspace\eimage.png")
